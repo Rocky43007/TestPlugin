d274b0cc4bb2a69009b7a2303980ad492e0da1ff
Rocky43007 <11457760+Rocky43007@users.noreply.github.com>
2021-01-10 08:41:47 +0300

    Update testplugin.java

--- src/rocky/testplugin/testplugin.java
+++ src/rocky/testplugin/testplugin.java
@@ -1,5 +1,7 @@
 package rocky.testplugin;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class testplugin extends JavaPlugin{
@@ -9,5 +11,16 @@ public class testplugin extends JavaPlugin{
     }
     @Override
     public void onDisable(){
+    }
+    @Override
+    public boolean onCommand(CommandSender sender,
+                             Command command,
+                             String label,
+                             String[] args) {
+        if (command.getName().equalsIgnoreCase("test")) {
+            sender.sendMessage("You ran /test!");
+            return true;
+        }
+        return false;
     }
 }
